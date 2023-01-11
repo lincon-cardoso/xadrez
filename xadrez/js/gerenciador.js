b6ed979ea65ba3da04a22a061afc3be168a7e1e9
Lincon Cardoso <59518299+lincon-cardoso@users.noreply.github.com>
2023-01-11 01:58:37 -0300

    atualização

--- xadrez/js/gerenciador.js
+++ xadrez/js/gerenciador.js
@@ -129,15 +129,13 @@ rainhaBranca.style.alignItems = 'center';
 rainhaBranca.style.textAlign = 'center';
 rainhaBranca.innerHTML = '&#9813; ';
 
-
-//rei branco e preto 
+//rei branco e preto
 
 let reiBranco = document.getElementById('quadrado-' + 0 + '-' + 4);
 reiBranco.style.fontSize = '80px';
 reiBranco.style.alignItems = 'center';
 reiBranco.style.textAlign = 'center';
 reiBranco.innerHTML = '&#9818; ';
-
 
 let reiPreto = document.getElementById('quadrado-' + 7 + '-' + 4);
 
@@ -146,45 +144,46 @@ reiPreto.style.alignItems = 'center';
 reiPreto.style.textAlign = 'center';
 reiPreto.innerHTML = '&#9812; ';
 
-
-// for (const quadrado of quadrados) {
-//   quadrado.addEventListener('click', (event) => {
-//     console.log(event.target);
-//   });
-// }
-
-//Criar movimentação
-
-// console.log(event.target.textContent);
 const quadrados = document.querySelectorAll('.quadrado');
-
-// Define the cordenadas variable outside the event listener
 
 for (const quadrado of quadrados) {
   quadrado.addEventListener('click', (event) => {
-    let cordenadas = cordenadaMapa(event);
+    // let cordenadas = cordenadaMapa(event);
 
     cordenadaMapa(event);
     movePeca();
-    // console.log(cordenadas.id, cordenadas.peca);
-
-
-
-
-    
   });
 }
 
-
 function movePeca() {
-  const cordenadas = JSON.parse(localStorage.getItem('key'));
+  let cordenadas = JSON.parse(localStorage.getItem('key'));
+  if (!cordenadas) return;
+
+  const peca = cordenadas.peca;
+  const id = cordenadas.id;
+
+  let copyDiv = document.createElement('div');
+  copyDiv.innerHTML = peca;
+  copyDiv.classList = 'quadrado';
+  copyDiv.id = id;
+  copyDiv.setAttribute('peca', peca);
+
   
-  let peca = cordenadas.peca;
-  let id = cordenadas.id;
- 
-  const div = document.createElement('div')
- 
-  console.log(div)
+  document.body.appendChild(copyDiv);
+  
+//   document.body.addEventListener("click", function(event){
+//     if(event.target.classList.contains('quadrado')){
+//         const peca = event.target.getAttribute('peca');
+//         const id = event.target.id;
+//         let copyDiv = document.createElement('div');
+//         copyDiv.innerHTML = peca;
+//         copyDiv.classList = 'quadrado';
+//         copyDiv.id = id;
+//         copyDiv.setAttribute('peca', peca);
+//         event.target.parentNode.appendChild(copyDiv);
+//     }
+// });
+
 }
 
 
@@ -195,7 +194,7 @@ function cordenadaMapa(event) {
 
   const cordenadas = {
     id: idLocal,
-    peca: peca
+    peca: peca,
   };
 
   localStorage.setItem('key', JSON.stringify(cordenadas));
@@ -208,7 +207,6 @@ function cordenadaMapa(event) {
 
 //   return setPosicaoPeca;
 // }
-
 
 // function criaPeca(idPeca,elemento) {
 
@@ -223,18 +221,129 @@ function cordenadaMapa(event) {
 // console.log(idPeca)
 // vazio(idPeca);
 // function vazio(idPeca) {
-  //   if (idPeca === '') {
-    //     console.log("nao tem peça");
-    //   } else {
-      //     console.log("tem peça");
-      //   }
-      // }
-      
-      // let = novaPeca = document.createElement('div');
-      // novaPeca.className = 'quadrado';
-      // novaPeca.id = idPeca.id;
-      // novaPeca.appendChild(document.querySelector(idPeca));
-      // let quadrado = document.createElement('div');
-      // quadrado.className = 'quadrado';
-      // quadrado.id = 'quadrado-' + i + '-' + j; // adiciona o atributo id ao elemento
-      // tabuleiroDOM.appendChild(quadrado);
+//   if (idPeca === '') {
+//     console.log("nao tem peça");
+//   } else {
+//     console.log("tem peça");
+//   }
+// }
+
+// let = novaPeca = document.createElement('div');
+// novaPeca.className = 'quadrado';
+// novaPeca.id = idPeca.id;
+// novaPeca.appendChild(document.querySelector(idPeca));
+// let quadrado = document.createElement('div');
+// quadrado.className = 'quadrado';
+// quadrado.id = 'quadrado-' + i + '-' + j; // adiciona o atributo id ao elemento
+// tabuleiroDOM.appendChild(quadrado);
+
+// console.log(cordenadas.id, cordenadas.peca);
+// Define the cordenadas variable outside the event listener
+// for (const quadrado of quadrados) {
+//   quadrado.addEventListener('click', (event) => {
+//     console.log(event.target);
+//   });
+// }
+
+//Criar movimentação
+
+// console.log(event.target.textContent);
+// function createAndAppendElement(peca, id) {
+//   // Create the div element
+//   const div = document.createElement('div')
+//   div.className = id;
+//   div.innerHTML = peca;
+
+//   // Select the element where you want to append the new div
+//   const container = document.querySelectorAll('.quadrado');
+
+//   // Append the new div to the container
+//   container.appendChild(div);
+// }
+// const novapeca = document.createElement('div');
+// novapeca.className = ;
+// novapeca.innerText = id;
+
+//  console.log(novapeca.className)
+
+//   const newDiv = document.createElement('div');
+//   newDiv.classList = 'quadrado'
+//   newDiv.id = id;
+//   newDiv.textContent= id
+//   newDiv.innerText = peca;
+
+// // return newDiv
+
+// function createCopy() {
+//   //recupera peça guardada
+//   let storedPiece = JSON.parse(localStorage.getItem("key"));
+//   if (!storedPiece) return;
+
+//   // seleciona a peça guardada
+//   let piece = storedPiece.peca;
+
+//   // cria uma nova div
+//   let copyDiv = document.createElement("div");
+
+//   // adiciona o conteúdo da peça selecionada à nova div
+//   copyDiv.innerHTML = piece;
+
+//   // adiciona a nova div ao documento
+//   document.body.appendChild(copyDiv);
+// }
+
+// function movePeca() {
+//   if(!localStorage.getItem("key")) return;
+
+//   const cordenadas = JSON.parse(localStorage.getItem("key"));
+//   const peca = cordenadas.peca;
+//   const  id = cordenadas.id;
+
+//   let div = document.createElement("div");
+//   div.classList = "quadrado";
+//   div.id = id;
+//   div.setAttribute("peca", peca);
+
+//   let parent = document.querySelector("#" + id);
+//   if (!parent) return;
+//   parent.parentNode.replaceChild(div, parent);
+
+// }
+
+// function createCopy() {
+//   //recupera peça guardada
+//   let storedPiece = JSON.parse(localStorage.getItem("key"));
+//   if (!storedPiece) return;
+
+//   // seleciona a peça guardada
+//   let piece = storedPiece.peca;
+
+//   // cria uma nova div
+//   let copyDiv = document.createElement("div");
+
+//   // adiciona o conteúdo da peça selecionada à nova div
+//   copyDiv.innerHTML = piece;
+
+//   // adiciona a nova div ao documento
+//   document.body.appendChild(copyDiv);
+// }
+// console.log(copyDiv);
+
+// document.querySelectorAll('.quadrado').forEach(element => {
+//   element.addEventListener('click', updateElement);
+// });
+
+// function updateElement(event) {
+//   let cordenadas = JSON.parse(localStorage.getItem('key'));
+//   if (!cordenadas) return;
+
+//   let  peca = cordenadas.peca;
+//   let  id = cordenadas.id;
+
+//   // event.target.setAttribute('id', id);
+//   // event.target.setAttribute('peca', peca);
+//   // event.target.textContent = peca + ' '+ id;
+
+//   console.log(id);
+
+// }
